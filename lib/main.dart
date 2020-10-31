9f2f1cad9d0e447b799c785d2f88bcd57d99fa29
Craig <craig@oppkey.com>
2020-10-31 12:07:00 -0700

    added accent color

--- lib/main.dart
+++ lib/main.dart
@@ -37,6 +37,7 @@ class MyApp extends StatelessWidget {
       darkTheme: ThemeData(
         brightness: Brightness.dark,
         primaryColor: Colors.red[300],
+        accentColor: Colors.red[800],
       ),
       themeMode: ThemeMode.dark,
       routes: {
