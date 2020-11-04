72c51046b0941362e0a7f392e344a5186dc9e51e
Craig <craig@oppkey.com>
2020-11-03 17:57:11 -0800

    added photoview for files screen

--- lib/screens/files_screen.dart
+++ lib/screens/files_screen.dart
@@ -4,7 +4,7 @@ import 'package:theta_req_res/notifiers/camera_notifier.dart';
 import 'package:theta_req_res/windows/request_window.dart';
 import 'package:theta_req_res/windows/response_window.dart';
 import 'package:theta_req_res/screens/navigation_drawer.dart';
-import 'package:theta_req_res/camera_api/admin/delete_all_button.dart';
+import 'package:photo_view/photo_view.dart';
 import 'package:theta_req_res/camera_api/files/update_file_list_button.dart';
 
 class FilesScreen extends StatelessWidget {
@@ -32,57 +32,17 @@ class FilesScreen extends StatelessWidget {
                 //TODO: put list files button here
               ],
             ),
-            Row(
-              children: [
-                Container(
-                  width: 300.0,
-                  child: Center(
-                    child: Text(
-                      'Request',
-                      style: TextStyle(fontSize: 28),
-                    ),
-                  ),
-                ),
-                Container(
-                  width: 300.0,
-                  child: Center(
-                    child: Text(
-                      'Response',
-                      style: TextStyle(fontSize: 28),
-                    ),
-                  ),
-                ),
-                Container(
-                  width: 300.0,
-                  child: Center(
-                    child: Text(
-                      'Last Image',
-                      style: TextStyle(fontSize: 28),
-                    ),
-                  ),
-                ),
-              ],
-            ),
-            SizedBox(
-              height: 12,
-            ),
-            // request, response, and image windows
+
             Expanded(
-              child: Row(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  RequestWindow(),
-                  ResponseWindow(),
-                  Container(
-                    alignment: Alignment.topCenter,
-                    width: 300,
-                    // child: Image.network('https://i.imgur.com/lk6WHIW.jpg'),
-                    child: Image.network(
-                        '${Provider.of<CameraNotifier>(context).fileUri}?type=thumb'),
-                  ),
-                ],
+              child: Container(
+                alignment: Alignment.topCenter,
+                // child: Image.network('https://i.imgur.com/lk6WHIW.jpg'),
+                child: PhotoView(
+                  imageProvider: NetworkImage(
+                      '${Provider.of<CameraNotifier>(context).fileUri}'),
+                ),
               ),
-            )
+            ),
           ],
         ),
       ),
