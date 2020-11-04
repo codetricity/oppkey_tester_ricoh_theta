36fa4ffe8ab787ab9928cb8cb2f90fca3c0f7a0c
Craig <craig@oppkey.com>
2020-11-03 16:09:41 -0800

    updated takePicture button for mobile

--- lib/camera_api/take_picture.dart
+++ lib/camera_api/take_picture.dart
@@ -3,6 +3,7 @@ import 'package:provider/provider.dart';
 import 'package:http/http.dart' as http;
 import 'package:theta_req_res/notifiers/response_notifier.dart';
 import 'package:theta_req_res/notifiers/request_notifier.dart';
+import 'package:theta_req_res/notifiers/req_res_notifier.dart';
 import 'package:theta_req_res/utils/format_json.dart';
 import 'dart:convert';
 
@@ -19,15 +20,22 @@ class TakePictureButton extends StatelessWidget {
         var body =
             jsonEncode({'name': 'camera.takePicture', 'parameters': '{}'});
         var formattedResponse;
+        var formattedReqRes;
+
         try {
           var fullResponse = await http.post(url,
               headers: {"Content-Type": "application/json; charset=utf-8"},
               body: body);
           formattedResponse = formatJson('${fullResponse.body}');
+          formattedReqRes = 'REQUEST\n' +
+              fullResponse.request.toString() +
+              '\n\nRESPONSE\n' +
+              formattedResponse;
           context.read<ResponseNotifier>().updateResponse(formattedResponse);
           context
               .read<RequestNotifier>()
               .updateRequest('${fullResponse.request}');
+          context.read<ReqResNotifier>().updateReqRes(formattedReqRes);
         } catch (error) {
           context
               .read<ResponseNotifier>()
@@ -35,6 +43,9 @@ class TakePictureButton extends StatelessWidget {
           context
               .read<RequestNotifier>()
               .updateRequest('Request failed. \n\n Attempted URL:\n $url');
+          context
+              .read<ReqResNotifier>()
+              .updateReqRes('request failed.\n\n Error code:\n $error');
         }
       },
       child: Text('take picture'),
